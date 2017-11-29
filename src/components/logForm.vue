93eece0dba1774902e55aca597f88a620b402235
BOB <1971785019@qq.com>
2017-11-29 16:06:59 +0800

    公司

--- src/components/logForm.vue
+++ src/components/logForm.vue
@@ -4,7 +4,7 @@
       <div class="g-form-line">
         <span class="g-form-label">用户名：</span>
         <div class="g-form-input">
-          <input type="text" 
+          <input type="text"
           v-model="usernameModel" placeholder="请输入用户名">
         </div>
         <span class="g-form-error">{{ userErrors.errorText }}</span>
@@ -12,7 +12,7 @@
       <div class="g-form-line">
         <span class="g-form-label">密码：</span>
         <div class="g-form-input">
-          <input type="password" 
+          <input type="password"
           v-model="passwordModel" placeholder="请输入密码">
         </div>
         <span class="g-form-error">{{ passwordErrors.errorText }}</span>
